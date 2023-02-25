cc0c805398366bf15b661bdd6e1ec9d5704919fe
David Vetter <ddvetter23@gmail.com>
2023-02-25 13:06:02 -0600

    moved button for logging out

--- whatsapp-clone/client/src/components/Dashboard.jsx
+++ whatsapp-clone/client/src/components/Dashboard.jsx
@@ -9,7 +9,7 @@ export default function Dashboard({ id, logout }) {
   return (
     <div className="d-flex" style={{height: '100vh'}}>
       <Sidebar id={id} />
-      {/* <Button onClick={handleLogout}>Logout</Button> */}
+      <Button style={{position: 'absolute', bottom: 0, right: 0}}onClick={handleLogout}>Logout</Button>
     </div>
   );
 }
