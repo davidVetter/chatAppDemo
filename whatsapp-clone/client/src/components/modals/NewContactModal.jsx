c7720641bdcb59ebee4a62522e5cdb48115edd0c
David Vetter <ddvetter23@gmail.com>
2023-02-25 13:34:34 -0600

    added form to new contact modal

--- whatsapp-clone/client/src/components/modals/NewContactModal.jsx
+++ whatsapp-clone/client/src/components/modals/NewContactModal.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useRef } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 export default function NewContactModal() {
+  const idRef = useRef();
+  const nameRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+  }
   return (
-    <div>New Contact Modal</div>
+    <>
+      <Modal.Header closeButton>
+        Create Contact
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Id</Form.Label>
+            <Form.Control type='text' ref={idRef} required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='text' ref={nameRef} required />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </>
   )
 }
