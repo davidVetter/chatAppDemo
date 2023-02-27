2e32919fc1fa6b4842c89adcfaa4fc59ce1bee39
David Vetter <ddvetter23@gmail.com>
2023-02-27 12:27:09 -0600

    added text area input for messages

--- whatsapp-clone/client/src/components/Dashboard.jsx
+++ whatsapp-clone/client/src/components/Dashboard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
+import OpenConversations from "./OpenConversations";
 import Sidebar from "./Sidebar";
 
 export default function Dashboard({ id, logout }) {
+    const { selectedConversion } = useConversations();
   function handleLogout() {
     logout("");
   }
   return (
     <div className="d-flex" style={{height: '100vh'}}>
       <Sidebar id={id} />
-      <Button className="rounded-0" variant="secondary"  style={{position: 'absolute', bottom: 0, right: 0}}onClick={handleLogout}>Logout</Button>
+      {selectedConversion && <OpenConversations />}
+      <Button className="rounded-0" variant="secondary"  style={{position: 'absolute', top: 0, right: 0}}onClick={handleLogout}>Logout</Button>
     </div>
   );
 }
--- whatsapp-clone/client/src/components/OpenConversations.jsx
+++ whatsapp-clone/client/src/components/OpenConversations.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useState } from "react";
+import { Form, InputGroup } from "react-bootstrap";
 
 export default function OpenConversations() {
+  const [text, setText] = useState('');
   return (
-    <div>OpenConversations</div>
-  )
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto">
+
+      </div>
+      <Form>
+        <Form.Group className="m-2">
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              required
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: '75px', resize: 'none'}}
+            />
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
