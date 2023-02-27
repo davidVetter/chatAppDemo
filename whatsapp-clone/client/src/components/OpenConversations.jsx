f0a3e030ed4f7c1c5b48afc152f71869c0879a79
David Vetter <ddvetter23@gmail.com>
2023-02-27 15:40:00 -0600

    setup useCallback and conversations to display when selected

--- whatsapp-clone/client/src/components/OpenConversations.jsx
+++ whatsapp-clone/client/src/components/OpenConversations.jsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function OpenConversations() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const setRef = useCallback(node => {
+    if (node) {
+    node.scrollIntoView({ smooth: true})
+    }
+  }, []);
   const { sendMessage, selectedConversion } = useConversations();
 
   function handleSubmit(e) {
     e.preventDefault();
-    sendMessage(selectedConversion.recipients.map(r => r.id), text);
-    setText('');
+    sendMessage(
+      selectedConversion.recipients.map((r) => r.id),
+      text
+    );
+    setText("");
   }
+
   return (
     <div className="d-flex flex-column flex-grow-1">
       <div className="flex-grow-1 overflow-auto">
-
+        <div className="d-flex flex-column align-items-start justify-content-end px-3">
+          {selectedConversion.messages.map((message, index) => {
+            const lastMessage = selectedConversion.messages.length - 1 === index;
+            return (
+              <div ref={lastMessage ? setRef : null} key={index} className={`${message.fromMe ? 'align-self-end': ''}my-1 d-flex flex-column`}>
+                <div className={`${message.fromMe ? 'bg-primary text-white' : 'border'} rounded px-2 py-1`}>{message.text}</div>
+                <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>{message.fromMe ? "You" : message.senderName}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="m-2">
@@ -24,7 +43,7 @@ export default function OpenConversations() {
               required
               value={text}
               onChange={(e) => setText(e.target.value)}
-              style={{ height: '75px', resize: 'none'}}
+              style={{ height: "75px", resize: "none" }}
             />
             <Button type="submit">Send</Button>
           </InputGroup>
