9470137c4c1de5153ad9540e6241dc580e26cb5c
David Vetter <ddvetter23@gmail.com>
2023-02-28 10:08:13 -0600

    ran prettier on openConversations component

--- whatsapp-clone/client/src/components/OpenConversations.jsx
+++ whatsapp-clone/client/src/components/OpenConversations.jsx
@@ -4,9 +4,9 @@ import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function OpenConversations() {
   const [text, setText] = useState("");
-  const setRef = useCallback(node => {
+  const setRef = useCallback((node) => {
     if (node) {
-    node.scrollIntoView({ smooth: true})
+      node.scrollIntoView({ smooth: true });
     }
   }, []);
   const { sendMessage, selectedConversion } = useConversations();
@@ -25,11 +25,30 @@ export default function OpenConversations() {
       <div className="flex-grow-1 overflow-auto">
         <div className="d-flex flex-column align-items-start justify-content-end px-3">
           {selectedConversion.messages.map((message, index) => {
-            const lastMessage = selectedConversion.messages.length - 1 === index;
+            const lastMessage =
+              selectedConversion.messages.length - 1 === index;
             return (
-              <div ref={lastMessage ? setRef : null} key={index} className={`${message.fromMe ? 'align-self-end': ''}my-1 d-flex flex-column`}>
-                <div className={`${message.fromMe ? 'bg-primary text-white' : 'border'} rounded px-2 py-1`}>{message.text}</div>
-                <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>{message.fromMe ? "You" : message.senderName}</div>
+              <div
+                ref={lastMessage ? setRef : null}
+                key={index}
+                className={`${
+                  message.fromMe ? "align-self-end" : ""
+                }my-1 d-flex flex-column`}
+              >
+                <div
+                  className={`${
+                    message.fromMe ? "bg-primary text-white" : "border"
+                  } rounded px-2 py-1`}
+                >
+                  {message.text}
+                </div>
+                <div
+                  className={`text-muted small ${
+                    message.fromMe ? "text-right" : ""
+                  }`}
+                >
+                  {message.fromMe ? "You" : message.senderName}
+                </div>
               </div>
             );
           })}
